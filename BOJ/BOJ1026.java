b923f2d544ab11b40b804b5e80f6f86eb228c48b
jh9875 <qs9875@gmail.com>
2022-07-03 19:30:31 +0900

    220703

---
+++ BOJ/BOJ1026.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int n = Integer.parseInt(br.readLine());
+        int[] arrayA = new int[n];
+        int[] arrayB = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n ;i++) {
+            arrayA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n ;i++) {
+            arrayB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(this.getReslut(arrayA, arrayB, n));
+
+        br.close();
+    }
+
+    private int getReslut(int[] arrayA, int[] arrayB, int n) {
+        int result = 0;
+
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+
+        for (int i = 0; i < n; i++) {
+            result += arrayA[i] * arrayB[n - 1 - i];
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
