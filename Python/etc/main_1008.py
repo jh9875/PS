45df8b4912b876b2ae53e6d8ad6f7914041da5fe
JaeHoon Kim <qs9875@gmail.com>
2022-10-07 23:51:19 +0900

    boj commit 2022.10.07

---
+++ Python/etc/main_1008.py
@@ -0,0 +1,3 @@
+input_line = input().split(' ')
+print(int(input_line[0]) / int(input_line[1]))
+
---
+++ Python/etc/main_10430_1.py
@@ -0,0 +1,11 @@
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+input_line = input().split(' ')
+a = int(input_line[0])
+b = int(input_line[1])
+c = int(input_line[2])
+
+print((a + b) % c)
+print(((a % c) + (b % c)) % c)
+print((a * b) % c)
+print(((a % c) * (b % c)) % c)
+
